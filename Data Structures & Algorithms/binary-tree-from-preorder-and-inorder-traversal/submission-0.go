020269e96c0b2fe04759d83bc2cec711d0747bd4
aigeriam <aigerimabunusipova@gmail.com>
2026-09-12 17:56:21 +0500

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-0.go
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func buildTree(preorder []int, inorder []int) *TreeNode {
+    pre, in:=0, 0
+	var dfs func (int)*TreeNode
+	dfs=func(wall int) *TreeNode{
+		if pre>=len(preorder){
+			return nil
+		}
+		if wall==inorder[in]{
+			in++
+			return nil
+		}
+		root:=&TreeNode{Val: preorder[pre]}
+		pre++
+		root.Left=dfs(root.Val)
+		root.Right=dfs(wall)
+		return root
+	}
+	return dfs(math.MaxInt)
+}
